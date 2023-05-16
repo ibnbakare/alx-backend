12496500f48481fecb7d3ae3b316d8250dde108b
ridhwan <ridmartrite@gmail.com>
2023-05-16 01:55:32 +0100

    adding task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""Pagination helper function.
+"""
+Simple helper function
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Returns a given page and page size.
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    Returns index range from a given page and page size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
