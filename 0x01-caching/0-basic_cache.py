c42ea7e24d193ac0ffc39c5007475ecfcd84f724
ridhwan <ridmartrite@gmail.com>
2023-05-17 17:58:37 +0100

    adding task 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic caching module.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Storing and getting objects in a cache
+    """
+    def put(self, key, item):
+        """item added to the cache.
+        """
+        if key and item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets an item by key.
+        """
+        return self.cache_data.get(key, None)
